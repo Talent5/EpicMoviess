eb6cd2f93c1ddcfcf45273caa532cdf90d8b58ab
Talent Mundwa <talent.mundwa@students.uz.ac.zw>
2024-08-30 11:14:22 +0200

    refactor: Remove unnecessary code in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-
 export {Header} from "./Header"
 export {Footer} from "./Footer"
 export {Card} from "./Card"
-=======
-export {Header} from "./Header"
-export {Footer} from "./Footer"
-export {Card} from "./Card"
->>>>>>> origin/master
