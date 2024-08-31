396d589ac3f870827e6374b8d7f951c1099e67c6
Talent Mundwa <talent.mundwa@students.uz.ac.zw>
2024-08-31 13:41:07 +0200

    refactor: Update API configuration and Card component
    The API configuration in Link.txt was updated to include the API key and base URL for themoviedb.org. Additionally, the Card component in Card.js was modified to display movie information dynamically, including the movie title, overview, and poster image. These changes improve the functionality and user experience of the application.

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -6,16 +6,19 @@ export const MovieList = () => {
 
     useEffect (() => {
         async function fetchMovies() {
-            const response = await fetch();
+            const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=e466499a38fbb6c457506d31101b73b2");
             const data = await response.json();
             setMovies(data.results);
         }
-    })
+        fetchMovies();
+    }, []) 
     return (
         <main>
             <section className='max-w-7xl mx-auto py-7'>
                 <div className='flex justify-start flex-wrap'>
-                    <Card/>
+                    { movies.map((movie) =>(
+                        <Card key ={movie.id} movie={movie}/>
+                    ))}
                 </div>
             </section>
         </main>
