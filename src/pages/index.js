415e94d7c008b55d196df9ad78221539c743b52c
Talent Mundwa <talent.mundwa@students.uz.ac.zw>
2024-08-31 08:21:44 +0200

    refactor: Remove unnecessary code in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 export { MovieList } from "./MovieList";
 export { MovieDetail } from "./MovieDetail";
 export { Search } from "./Search";
-
 export { PageNotFound } from "./PageNotFound";
-=======
-export {MovieList} from "./MovieList"
-export {MovieDetail} from "./MovieDetail"
-export {Search} from "./Search"
-export {PageNotFound} from "./PageNotFound"
->>>>>>> origin/master
